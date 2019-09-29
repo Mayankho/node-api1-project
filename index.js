86b1ad46818bcff6c1ec2c92b0c45b83c7d39f40
Mayankho <yankhot96@gmail.com>
2019-09-28 21:28:38 -0400

    Start

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // implement your API here
+
+
+const express = require('express'); // This is how it is don from th common  js module
+
+const server = express();
+//This gives us an instance of a server powered by epress. 
+
+
