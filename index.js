7a27fb389f267b19297a03e30aa57420a8465931
Mayankho <yankhot96@gmail.com>
2020-04-07 23:43:16 -0400

    updated error from delete

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // implement your API here
 
-const database = require('./data/db');
+const db = require('./data/db');
 
 const express = require('express'); // This is how it is don from th common  js module
 
@@ -12,12 +12,52 @@ server.get('/', (req, res) => {
 });
 
 server.get('/api/users', (req, res) => {
-    res.send('Check the console!');
+    // If the req body is turning null then send an error respond with a http 500 erroro message
+    // Return  a json  element
+    db.find()
+    .then(users => {
+
+        res.status(200).json({
+            message: 'You made it buddy',
+        })
+        .catch(err => {
+            res.status(500).json({
+                err: "You can not get the users information"
+            })
+        }
+        )
+    })
 });
 
 server.get('/api/users/:id', (req,res) => {
     res.send('Users and id goes here');
 })
+
+server.post('/api/users', (req, res) => {
+    db.insert(user)
+    .then(newUser => {
+        res.status(200).json({
+        })
+        .catch(
+            ( err => {
+                res.status(400).json({
+                    err: "Please provide name and bio for user",
+                })
+            })
+        )
+    })
+})
+
+
+server.delete('/api/users/:id', (req,res) => {
+    db.remove(deleteUser)
+    .then(deleteUser => {
+        res.status(200).json({
+            Messsage : "deleted the selection",
+        })
+    })
+})
+
 
 
 
